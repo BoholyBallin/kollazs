e8358f0accd77f6d04401712a59beffc8097bfd6
Boholy Bálint <90762111+BoholyBallin@users.noreply.github.com>
2022-04-27 08:51:42 +0200

    Add files via upload

---
+++ kosar.js
@@ -0,0 +1,18 @@
+var plakat1=2000
+var plakat2=3000
+var plakat3=4000
+function hozzaad (termek){
+    db=Number(prompt("Mennyit?"))
+    localStorage:setItem(termek,db)
+
+}
+function torl (){
+    localStorage.clear()
+}
+function kosar (kosar)
+{
+var list""
+list +="<tr><th>név:</th>"
+list +="<tr><td>"+termek+"</td>"
+document.getElementById('kosar')
+}
